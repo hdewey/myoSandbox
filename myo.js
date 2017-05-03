d1d87dcdca3e5f265b5adbca624546763f3cbef9
Henry Dewey <henrydewey@Henrys-MacBook-Pro.local>
2017-05-03 13:50:12 -0500

    5/3/17

--- myo.js
+++ myo.js
@@ -1,8 +1,11 @@
 var Myo = require('myo');
 
-Myo.connect('com.hdewey.Sandbox', require('ws'));
-
+Myo.on("connected", function(data, timestamp) {
+  console.log("Myo successfully connected. Data: " + JSON.stringify(data) + ". Timestamp: " + timestamp + ".");
+});
 
 Myo.on("fingers_spread", function() {
   console.log("Fingers spread!");
 });
+
+Myo.connect('com.hdewey.Sandbox');
